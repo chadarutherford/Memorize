4d119eceb8793634b83dd6ff143cca515bc1ee03
Chad Rutherford <chad_rutherford@outlook.com>
2020-07-29 21:58:28 -0400

    Added Constants file.

---
+++ Memorize/Support/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+// Constants.swift
+// Memorize
+//
+// Created by Chad Rutherford on 7/29/20.
+// Copyright © 2020 chadarutherford.github.io. All rights reserved.
+//
+
+import UIKit
+
+enum Size {
+	static let cornerRadius: CGFloat = 10.0
+	static let lineWidth: CGFloat = 3.0
+	static let multiplier: CGFloat = 0.75
+}
