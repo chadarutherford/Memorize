c498fc65421504f672a27c8d13501b8a2a905013
Chad Rutherford <chad_rutherford@outlook.com>
2020-07-30 12:58:47 -0400

    Added Only extension to Array

--- Memorize/Support/Extensions/Array+Ext.swift
+++ Memorize/Support/Extensions/Array+Ext.swift
@@ -9,12 +9,18 @@
 import Foundation
 
 extension Array where Element: Identifiable {
-	func firstIndex(matching: Element) -> Int {
+	func firstIndex(matching: Element) -> Int? {
 		for index in 0 ..< self.count {
 			if self[index].id == matching.id {
 				return index
 			}
 		}
-		return 0 // TODO: bogus!
+		return nil
+	}
+}
+
+extension Array {
+	var only: Element? {
+		count == 1 ? first : nil
 	}
 }
